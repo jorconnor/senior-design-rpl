b0934be15af8f8a0389f9fd914510d8a9dcc1315
Unknown <trgasdaska@gmail.com>
2017-02-27 02:45:29 +0000

    Finalized the dependencies demo script

--- demoscripts/dependencies/cdependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Reads a c file whose path is specified as a command line argument
-# and returns the dependencies.
-
-echo "DEPENDENCIES:"
-rosie -wholefile -encode json c.dependencies $1 | sed 's/c\.//g' | jq '.[].subs[].dependency.subs[].header.text'
