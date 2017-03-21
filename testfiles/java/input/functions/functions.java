37b32f78df98b564ac176e57ba857e7d0155e9bd
Maya Shankar <mayashankar24@gmail.com>
2017-03-21 10:17:44 -0400

    more robust test file for java function calls

--- testfiles/java/input/functions/functions.java
+++ testfiles/java/input/functions/functions.java
@@ -1,3 +1,48 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2011, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jenkins.security;
+
+import hudson.Extension;
+import jenkins.util.SystemProperties;
+import hudson.Util;
+import hudson.model.Descriptor.FormException;
+import hudson.model.User;
+import hudson.model.UserProperty;
+import hudson.model.UserPropertyDescriptor;
+import hudson.security.ACL;
+import hudson.util.HttpResponses;
+import hudson.util.Secret;
+
+public class ApiTokenProperty extends UserProperty {
+
+    /**
+     * We don't let the external code set the API token,
+     * but for the initial value of the token we need to compute the seed by ourselves.
+     */
+    /*package*/ ApiTokenProperty(String seed) {
+        apiToken = Secret.fromString(seed);
+    }
     boolean matchesPassword(String password, String username) {
         String token = getApiTokenInsecure();
         // String.equals isn't constant time, but this is
@@ -45,3 +90,4 @@
     public UserProperty reconfigure(StaplerRequest req, JSONObject form) throws FormException {
         return this;
     }
+}
