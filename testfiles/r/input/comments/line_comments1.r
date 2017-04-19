10540d7238a40befb1529f2682b06846d3fb73cf
Maya Shankar <mayashankar24@gmail.com>
2017-04-19 11:17:36 -0400

    R: changed line endings

--- testfiles/r/input/comments/line_comments1.r
+++ testfiles/r/input/comments/line_comments1.r
@@ -13,7 +13,7 @@ class(y)
 
 # Q6
 a <- c(3, 2, 10)
-b <- c(1,3, 5) 
+b <- c(1,3, 5)
 cbind(a,b)
 
 #Q7
@@ -30,7 +30,7 @@ class(d+e)
 
 
 # Q 10
-f <- c(17, 14, 4, 5, 13, 12, 10) 
+f <- c(17, 14, 4, 5, 13, 12, 10)
 f[f > 10] <- 4
 
 # Q11, 12, 13, 14
@@ -49,12 +49,12 @@ sum(is.na(hw1_data$Ozone))
 # What is the mean of the Ozone column in this dataset? Exclude missing values (coded as NA) from this calculation.
 mean(na.omit(hw1_data$Ozone))
 
-# Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. 
+# Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90.
 # What is the mean of Solar.R in this subset?
 hw1_data.sub1 <- subset(hw1_data, Ozone > 31 & Temp > 90)
 mean(hw1_data.sub1$Solar.R)
 
-# What is the mean of "Temp" when "Month" is equal to 6? 
+# What is the mean of "Temp" when "Month" is equal to 6?
 hw1_data.sub2 <- subset(na.omit(hw1_data), Month == 6)
 mean(hw1_data.sub2$Temp)
 
